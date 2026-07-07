e2090f326a5d2e34a4ceb6944a220773ec572429
Ian <iankimfaa1207@gmail.com>
2026-07-08 02:36:01 +0800

    chore: ignore AI files and remove scratch scripts

--- check_dept.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-$employees = App\Models\Employee::where('department_id', 4)->with('user.roles')->get();
-echo json_encode($employees);
